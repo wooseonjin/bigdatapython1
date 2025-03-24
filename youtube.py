cd0a81cde3bb861bfeac886531f841f70f30e7a2
wooseonjin <p330912345@gmail.com>
2025-03-24 06:48:43 +0000

    유튜브 인기 급상승

--- youtube.py
+++ youtube.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -14,29 +15,30 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 service = Service("chromedriver")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# 멜론 차트 페이지 접속
-url = "https://www.melon.com/chart/index.htm"
+# 유튜브 인기 급상승 페이지 접속
+url = "https://www.youtube.com/feed/trending"
 driver.get(url)
 
 # 페이지 로딩 대기
-time.sleep(3)
+time.sleep(5)
 
-# 곡 제목 가져오기
-titles = driver.find_elements(By.CSS_SELECTOR, "div.ellipsis.rank01 > span > a")
-# 아티스트 가져오기
-artists = driver.find_elements(By.CSS_SELECTOR, "div.ellipsis.rank02 > span")
+# 동영상 제목 가져오기
+titles = driver.find_elements(By.CSS_SELECTOR, "h3.style-scope.ytd-video-renderer")
+
+# 조회수 가져오기
+views = driver.find_elements(By.CSS_SELECTOR, "span.inline-metadata-item.style-scope.ytd-video-meta-block")
 
 # 결과 저장 및 출력
-melon_chart = []
+trending_videos = []
 for i in range(len(titles)):
-    rank = i + 1
     title = titles[i].text
-    artist = artists[i].text
-    melon_chart.append(f"{rank}위: {title} - {artist}")
+    if i * 2 + 1 < len(views):  # 조회수만 필터링 (조회수와 업로드 날짜가 같이 있음)
+        view_count = views[i * 2 + 1].text
+        trending_videos.append(f"{i+1}위: {title} - {view_count}")
 
 # 출력
-for song in melon_chart:
-    print(song)
+for video in trending_videos:
+    print(video)
 
 # 브라우저 종료
 driver.quit()
