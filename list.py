679e4fdf45cec1ecdc59ba292361e7385efa7ada
wooseonjin <p330912345@gmail.com>
2025-03-31 06:42:51 +0000

    리스트

---
+++ list.py
@@ -0,0 +1,32 @@
+import
+songs = ["a노래", "b노래", "c노래", "d노래"]
+print(songs)
+print(songs[0])
+print(songs[1])
+print(songs[2])
+print(songs[3])
+print(songs[4])
+print(songs[5])
+print(songs[6])
+print(songs[7])
+print(songs[8])
+print(songs[9])
+print(songs[10])
+
+for i in range(1, 11):
+    print(i)
+
+for song in songs:
+    print(song)
+
+print("AI야 노래 한곡 만 추천해줘")
+print("알겠습니다. 제가 열심히 분석해서 고객님께 노래를 한곡 추천합니다.")
+
+#리스트를 쓰는 이유
+song1 = "a노래"
+song2 = "b노래"
+song3 = "C노래"
+
+#print(songs)
+#print(songs)
+#print(songs)
