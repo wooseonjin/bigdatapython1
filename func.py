3d7994ee95230d68fb377e29f7b055537bec840a
wooseonjin <p330912345@gmail.com>
2025-04-14 11:09:09 +0000

    함수활용

--- func.py
+++ func.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import random
 import time
 
 def m100(a):
@@ -102,6 +103,7 @@ def m_random(d):
     print(d)
     time.sleep(1)
     print("[좋아요! 제가 열심히 찾아서 사용자님께 노래를 한 곡 추천할게요.]")
+
     url = 'https://www.melon.com/chart/index.htm'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -116,4 +118,14 @@ def m_random(d):
 
         for song in songs:
             rank = song.select_one('span.rank').text.strip()
-            title = song.select_one('div.ellipsis.rank01 a').text
+            title = song.select_one('div.ellipsis.rank01 a').text.strip()
+            artist = song.select_one('div.ellipsis.rank02 a').text.strip()
+            song_list.append((rank, title, artist))
+
+        random_song = random.choice(song_list)
+        time.sleep(1)
+        print(f"[이 노래가 좋을 거 같아요!]")
+        time.sleep(1)
+        print(f'\n[추천 곡: {random_song[1]} | 아티스트: {random_song[2]}]')
+    else:
+        print(f'[웹 페이지를 가져오는 데 실패했어요. T.T | 상태 코드: {response.status_code}]')
