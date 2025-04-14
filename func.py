fbce91b24703fbacec8a96b1a51f0200c8da87f0
wooseonjin <p330912345@gmail.com>
2025-04-14 11:05:13 +0000

    함수활용

--- func.py
+++ func.py
@@ -97,3 +97,23 @@ def m000(a, c):
         artist = song.select_one('div.ellipsis.rank02 a').text.strip()
 
         print(f'{rank}위 | 제목: {title} | 아티스트: {artist}')
+
+def m_random(d):
+    print(d)
+    time.sleep(1)
+    print("[좋아요! 제가 열심히 찾아서 사용자님께 노래를 한 곡 추천할게요.]")
+    url = 'https://www.melon.com/chart/index.htm'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        songs = soup.select('tr[data-song-no]')
+        song_list = []
+
+        for song in songs:
+            rank = song.select_one('span.rank').text.strip()
+            title = song.select_one('div.ellipsis.rank01 a').text
